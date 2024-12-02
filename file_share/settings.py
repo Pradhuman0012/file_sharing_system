b6b80e6153097f6c9c4531924f0b32169a8201a4
pradhuman tamboli <pradhumantamboli12@gmail.com>
2024-12-02 14:29:11 +0530

    Add local host to ALLOWED_HOSTS and JWT token endpoints

--- file_share/settings.py
+++ file_share/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tambolipradhuman123.pythonanywhere.com',]
+ALLOWED_HOSTS = ['tambolipradhuman123.pythonanywhere.com','127.0.0.1',]
 
 
 # Application definition
--- file_share/urls.py
+++ file_share/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
+    path('apitoken/', TokenObtainPairView.as_view(),name="apitoken" ),
+    path('refreshtoken/', TokenRefreshView.as_view(),name="refreshtoken" ),
+    path('verifytoken/', TokenVerifyView.as_view(),name="verifytoken" ),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
